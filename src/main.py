41288d351c4c36ed9e463a5b05cec7bb27af4edf
Dipanjan Patra <patraanjan23@gmail.com>
2020-10-03 18:22:28 +0530

    initial commit, added main file

---
+++ src/main.py
@@ -0,0 +1,16 @@
+print("Welcome to MY PROGRAM")
+print("Python is awesome!")
+
+# def f(x):
+#     return 2**x
+
+# def f2(x):
+#     sum = 0
+#     with open("data\\num.txt") as input:
+#         numbers = input.readlines()
+#         for n in numbers:
+#             sum += int(n) ** x
+#     return sum
+
+x = int(input("enter a number: "))
+print("f(x) = " + str(2**x))
